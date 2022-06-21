acf8a6c94e50c75d20f77d1dbe4d257bf91431f0
johannesbosse <johannes.bosse@rwth-aachen.de>
2022-06-21 12:09:52 +0200

    Added Fixed Point Test

--- test/simplest_model.jl
+++ test/simplest_model.jl
@@ -10,5 +10,14 @@ p = [rand(), rand()*2000, rand()*1e-8]
 u0 = rand()*200
 t = rand()*2
 prob = ODEProblem(zero_d_ebm, u0, t, p)
-sol = solve(prob)
-@test all(sol .≈ solve(SimplestModel(;u0=u0,p=p,T=t)))
+sol_1 = solve(prob)
+sol_2 = solve(SimplestModel(;u0=u0,p=p,T=t))
+@test all(sol_1 .≈ sol_2)
+
+function fixed_point(p,ε)
+    α, S₀, σ = p
+    return (((1-α)*S₀)/(4*ε*σ))^(1/4)
+end
+
+sol_3 = solve(SimplestModel(;u0=u0, p=p, T=1000.))
+@test abs(sol_3.u[end] - fixed_point(p,1)) < 0.1
